05723531840a23a4d834cdd40673645d1cc15781
Leftyxiv <manny@aledoux.net>
2021-03-24 18:16:13 -0400

    got the navbar in and working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css"
 
+import Navbar from './components/Navbar'
+
+
 function App() {
-  return <h1>Portfolio project</h1>;
+  return(
+    <Navbar />
+    ) 
+    
 }
 
 export default App;
