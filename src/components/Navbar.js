9a97242b9cea3da740b1aeef60ada6f434381968
Leftyxiv <manny@aledoux.net>
2021-03-25 06:18:32 -0400

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,8 @@
+import { faPray } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
@@ -16,8 +20,19 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <FontAwesomeIcon icon={faBars} style={{ color: "white" }} />
         </button>
+        {/* <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+    <span class="navbar-toggler-icon"></span>
+</button> */}
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
